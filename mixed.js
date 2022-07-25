0eaceb97940ebd2bef12e29e789abeb3bdf46665
caiodjo <caiodjo.fernandes@gmail.com>
2022-07-25 00:55:19 -0400

    add functions generations

--- mixed.js
+++ mixed.js
@@ -19,3 +19,24 @@ const ninja = {
     },
 }
 
+const gerarNome = (obj) =>{
+    const nome = obj.nomes[Math.floor(Math.random()*obj.nomes.length)];
+    console.log(nome)
+    return nome;
+}
+const gerarClasse = (obj) =>{
+    const classe = obj.classes[Math.floor(Math.random()*obj.classes.length)];
+    console.log(classe)
+    return classe;
+}
+const gerarAlcunha = (obj) =>{
+    const alcunha = obj.alcunhas[Math.floor(Math.random()*obj.alcunhas.length)];
+    console.log(alcunha)
+    return alcunha;
+}
+
+const gerarNinja = (nome, classe, alcunha) =>{
+    return `Seu nome ninja é: ${nome} ${classe} ${alcunha}.`
+}
+
+console.log(gerarNinja(gerarNome(ninja),gerarClasse(ninja),gerarAlcunha(ninja)));
